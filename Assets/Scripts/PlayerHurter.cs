6ba577eefe51a7e6cc8c7968bf105499faa62d4f
Sapphon <sapphon@gmail.com>
2023-02-13 22:12:29 -0500

    PlayerHurter calls GameManager when collider triggered by player

---
+++ Assets/Scripts/PlayerHurter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController.Examples;
+using UnityEngine;
+
+public class PlayerHurter : MonoBehaviour
+{
+
+    public void Start()
+    {
+        this.GetComponent<Collider>().isTrigger = true;
+    }
+
+    bool isCollisionWithPlayer(Collider triggeredCollider)
+    {
+        return triggeredCollider.gameObject.GetComponent<ExampleCharacterController>() != null;
+    }
+
+    void OnTriggerEnter(Collider triggeredCollider)
+    {
+        if (isCollisionWithPlayer(triggeredCollider))
+        {
+            Debug.Log("Killing player via collider!");
+            GameManager.instance.Respawn();
+        }
+    }
+}
