15fa2a6db35c18bec1639d23829ba7292daccb02
Sapphon <sapphon@gmail.com>
2023-02-18 23:36:12 -0500

    Fix duplicate UIController for KeyStatus

--- Assets/Scripts/PlayerStatusEffect/KeyStatusUIController.cs
+++ Assets/Scripts/PlayerStatusEffect/KeyStatusUIController.cs
@@ -1,45 +1,42 @@
 using TMPro;
 using UnityEngine;
 
-namespace PlayerStatusEffect
+public class KeyStatusUIController : MonoBehaviour
 {
-    public class KeyStatusUIController : MonoBehaviour
+    public TextMeshProUGUI textMesh;
+    private float lastAffectedTime = -1;
+    public Color alertColor = Color.green;
+
+    void Start()
     {
-        public TextMeshProUGUI textMesh;
-        private float lastAffectedTime = -1;
-        public Color alertColor = Color.green;
-
-        void Start()
+        if (textMesh == null)
         {
-            if (textMesh == null)
-            {
-                this.textMesh = this.GetComponent<TextMeshProUGUI>();
-            }
+            this.textMesh = this.GetComponent<TextMeshProUGUI>();
         }
+    }
 
-        bool affectedNow()
+    bool affectedNow()
+    {
+        float timeSinceLastAffected = Time.time - this.lastAffectedTime;
+        return timeSinceLastAffected > 0 && timeSinceLastAffected < 1;
+    }
+
+    void LateUpdate()
+    {
+        if (this.affectedNow())
         {
-            float timeSinceLastAffected = Time.time - this.lastAffectedTime;
-            return timeSinceLastAffected > 0 && timeSinceLastAffected < 1;
+            Debug.Log("Setting alertColor to " + alertColor);
+            this.textMesh.color = alertColor;
         }
-
-        void LateUpdate()
+        else
         {
-            if (this.affectedNow())
-            {
-                this.textMesh.color = alertColor;
-            }
-            else
-            {
-                if(this.textMesh)
-                    this.textMesh.color = Color.clear;
-            }
-
+            if (this.textMesh)
+                this.textMesh.color = Color.clear;
         }
+    }
 
-        public void showStatusNextFrame()
-        {   
-            this.lastAffectedTime = Time.time;
-        }
+    public void showStatusNextFrame()
+    {
+        this.lastAffectedTime = Time.time;
     }
 }
