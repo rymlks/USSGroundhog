196a0e04b3182f9fcf572f0647c2f13eac97b0d6
Sapphon <sapphon@gmail.com>
2023-04-12 12:44:51 -0400

    Remove extraneous creation of emission data from blood spatter

--- Assets/Scripts/Consequences/BloodSpatterConsequence.cs
+++ Assets/Scripts/Consequences/BloodSpatterConsequence.cs
@@ -11,19 +11,13 @@ public class BloodSpatterConsequence : MonoBehaviour, IConsequence
     public void execute(TriggerData? data)
     {
 
-        var emitParams = new ParticleSystem.EmitParams();
         Vector3 positionToSplatAt = this.gameObject.transform.position; 
         if (data?.triggerLocation != null)
         {
             positionToSplatAt = data.triggerLocation.Value;
         }
 
-        emitParams.position = positionToSplatAt;
-
-
         spatterParticleSystem.transform.position = positionToSplatAt;
-
-        Debug.Log("bloodSpatter.transform.position is equal to " + positionToSplatAt);
 
         spatterParticleSystem.Play();
 
