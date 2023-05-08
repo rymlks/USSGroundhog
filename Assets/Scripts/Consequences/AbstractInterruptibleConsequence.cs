6c109b61e85c2c8973eb8cd0ee4b0e52ee221a30
Sapphon <sapphon@gmail.com>
2023-05-07 23:44:49 -0400

    Begin removing timed effects that are not consequences

---
+++ Assets/Scripts/Consequences/AbstractInterruptibleConsequence.cs
@@ -0,0 +1,25 @@
+#nullable enable
+using Triggers;
+
+namespace Consequences
+{
+    public abstract class AbstractInterruptibleConsequence : AbstractConsequence, IInterruptibleConsequence
+    {
+        protected bool started = false;
+        
+        public void Interrupt(TriggerData? data)
+        {
+            this.started = false;
+        }
+
+        public override void execute(TriggerData? data)
+        {
+            this.started = true;
+        }
+    }
+
+    public interface IInterruptibleConsequence : IConsequence
+    {
+        public void Interrupt(TriggerData? data);
+    }
+}
