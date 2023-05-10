1dc339f8176d6733baa19d5d3215e1a29f417e21
Sapphon <sapphon@gmail.com>
2023-05-10 00:57:37 -0400

    Add permanent items gathered to score screen

--- Assets/Scripts/UI/ScoreScreenUIController.cs
+++ Assets/Scripts/UI/ScoreScreenUIController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Analytics;
 using Managers;
 using UnityEngine;
@@ -23,8 +26,14 @@ namespace UI
                                  "\r\n" +
                                  "Company Time Spent: " + levelScore.elapsedTime + "\r\n\r\n" +
                                  "Overall Performance Rating: " + levelScore.performancePercentage + "%\r\n" +
-                                 "Performance Comments: " + levelScore.performanceComments;
+                                 "Performance Comments: " + levelScore.performanceComments + "\r\n" +
+                                 "Items Gathered: " + getItemsGatheredString(levelScore);
 
+        }
+
+        protected static string getItemsGatheredString(LevelScore levelScore)
+        {
+            return String.Concat(levelScore.gatheredPermanentItems.Select(item => item + "\r\n"));
         }
     }
 }
