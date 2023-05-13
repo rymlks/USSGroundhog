14f7553f8d556dd6fd790c9665d5fbb36fb0df67
Sapphon <sapphon@gmail.com>
2023-05-13 03:42:05 -0400

    Camera priority is now a MainMenu.cs concern

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cinemachine;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +9,13 @@ namespace UI
 {
     public class MainMenu : MonoBehaviour
     {
+        protected List<CinemachineVirtualCamera> allMenuCameras;
+
+        void Start()
+        {
+            this.allMenuCameras = FindObjectsOfType<CinemachineVirtualCamera>().ToList();
+        }
+
         public void LoadFirstLevel()
         {
             SceneManager.LoadScene("Level 1");
@@ -17,6 +27,45 @@ namespace UI
 #if UNITY_EDITOR
             EditorApplication.ExitPlaymode();
 #endif
+        }
+
+        public void FocusMenu(string menuName)
+        {
+            CinemachineVirtualCamera found = GameObject.Find(menuName + "MenuVirtualCamera")
+                .GetComponentInChildren<CinemachineVirtualCamera>();
+            if (found == null)
+            {
+                Debug.Log("Cannot focus on menu " + menuName + "; menu camera not found");
+            }
+            else
+            {
+                this.allMenuCameras.ForEach(virtualCamera =>
+                {
+                    if (virtualCamera == found)
+                    {
+                        setGreaterCameraPriority(virtualCamera);
+                    }
+                    else
+                    {
+                        setLesserCameraPriority(virtualCamera);
+                    }
+                });
+            }
+        }
+
+        private static void setGreaterCameraPriority(CinemachineVirtualCamera virtualCamera)
+        {
+            setCameraPriority(virtualCamera, 20);
+        }
+
+        private static void setLesserCameraPriority(CinemachineVirtualCamera virtualCamera)
+        {
+            setCameraPriority(virtualCamera, 10);
+        }
+
+        private static void setCameraPriority(CinemachineVirtualCamera virtualCamera, int priority)
+        {
+            virtualCamera.Priority = priority;
         }
     }
 }
