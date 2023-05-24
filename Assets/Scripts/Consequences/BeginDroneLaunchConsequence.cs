1a2b1f6f97820f33c872bc405fb2fd821d7d4f27
Sapphon <sapphon@gmail.com>
2023-05-24 02:16:27 -0400

    Proper namespace for BeginDroneLaunchConsequence

--- Assets/Scripts/Consequences/BeginDroneLaunchConsequence.cs
+++ Assets/Scripts/Consequences/BeginDroneLaunchConsequence.cs
@@ -1,59 +1,59 @@
 #nullable enable
-using System.Collections;
-using System.Collections.Generic;
-using Consequences;
 using Triggers;
 using UnityEngine;
 
-public class BeginDroneLaunchConsequence : MonoBehaviour, IConsequence
+namespace Consequences
 {
-
-    public GameObject drone;
-    public GameObject dronePrefab;
-    public GameObject explosionPrefab;
-
-    protected bool started = true;
-    protected float explosionTime = -1f;
-
-    protected bool arePrerequisitesMet()
+    public class BeginDroneLaunchConsequence : MonoBehaviour, IConsequence
     {
-        return false;
-    }
 
-    void Update()
-    {
-        if (this.explosionTime > 0 && this.explosionTime <= Time.time)
+        public GameObject drone;
+        public GameObject dronePrefab;
+        public GameObject explosionPrefab;
+
+        protected bool started = true;
+        protected float explosionTime = -1f;
+
+        protected bool arePrerequisitesMet()
         {
-            CauseExplosion();
-            this.explosionTime = -1f;
-            Destroy(drone);
+            return false;
         }
-    }
 
-    private void CauseExplosion()
-    {
-        GameObject.Instantiate(explosionPrefab, drone.transform.position, Quaternion.identity);
-        Destroy(this.drone);
-    }
-
-    public void Execute(TriggerData? data)
-    {
-        this.BeginLaunch(drone);
-        if (!this.arePrerequisitesMet())
+        void Update()
         {
-            ScheduleExplosion();
+            if (this.explosionTime > 0 && this.explosionTime <= Time.time)
+            {
+                CauseExplosion();
+                this.explosionTime = -1f;
+                Destroy(drone);
+            }
         }
-    }
 
-    private void ScheduleExplosion()
-    {
-        this.explosionTime = Time.time + 7.5f;
-    }
+        private void CauseExplosion()
+        {
+            GameObject.Instantiate(explosionPrefab, drone.transform.position, Quaternion.identity);
+            Destroy(this.drone);
+        }
 
-    private void BeginLaunch(GameObject toLaunch)
-    {
-        this.started = true;
-        MoveObjectAlongWaypointsConsequence movement = toLaunch.GetComponent<MoveObjectAlongWaypointsConsequence>();
-        movement.Execute(null);
+        public void Execute(TriggerData? data)
+        {
+            this.BeginLaunch(drone);
+            if (!this.arePrerequisitesMet())
+            {
+                ScheduleExplosion();
+            }
+        }
+
+        private void ScheduleExplosion()
+        {
+            this.explosionTime = Time.time + 7.5f;
+        }
+
+        private void BeginLaunch(GameObject toLaunch)
+        {
+            this.started = true;
+            MoveObjectAlongWaypointsConsequence movement = toLaunch.GetComponent<MoveObjectAlongWaypointsConsequence>();
+            movement.Execute(null);
+        }
     }
 }
