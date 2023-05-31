18fac0005539cc67e32feb76ee7c944c0031fe20
Roland J Jarvis <Rjarvis87@gmail.com>
2023-05-31 00:08:09 -0400

    Main Panel buttons now have sound;

--- Assets/Scripts/Audio/PlayButtonSound.cs
+++ Assets/Scripts/Audio/PlayButtonSound.cs
@@ -1,30 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+namespace Audio
 {
-    public AudioClip mouseEnter, mouseExit, confirmClick;
-    public AudioSource audioSource;
+    public class PlayButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+    {
+        public AudioClip mouseEnter, mouseExit, confirmClick;
+        public AudioSource audioSource;
 
-    public void OnPointerEnter(PointerEventData data) {
-        ChangeClip(mouseEnter);
-        PlayClip();
-    }
+        public void OnPointerEnter(PointerEventData data) {
+            ChangeClip(mouseEnter);
+            PlayClip();
+        }
     
-    public void OnPointerExit(PointerEventData data) {
-        ChangeClip(mouseExit);
-        PlayClip();
-    }
+        public void OnPointerExit(PointerEventData data) {
+            ChangeClip(mouseExit);
+            PlayClip();
+        }
     
-    public void OnPointerClick(PointerEventData data) {
-        ChangeClip(confirmClick);
-        PlayClip();
-    }
+        public void OnPointerClick(PointerEventData data) {
+            ChangeClip(confirmClick);
+            PlayClip();
+        }
     
-    void ChangeClip(AudioClip clip) => audioSource.clip = clip;
-    void PlayClip() => audioSource.Play();
+        private void ChangeClip(AudioClip clip) => audioSource.clip = clip;
+        private void PlayClip() => audioSource.Play();
+    }
 }
