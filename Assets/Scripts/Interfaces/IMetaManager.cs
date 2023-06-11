cf27ad17c666201ab51849ee0138942a51c328cf
Sapphon <sapphon@gmail.com>
2023-06-11 14:01:50 -0400

    Remove unused interfaces

--- Assets/Scripts/Interfaces/IMetaManager.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Interfaces
-{
-    public interface IMetaManager
-    {
-        UnityEngine.MonoBehaviour GetManager<MonoBehaviour>(Predicate<UnityEngine.MonoBehaviour> manager);
-    }
-}
