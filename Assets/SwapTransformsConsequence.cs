2bd094a48cf084438e301e7a74dd6f38900dd330
Sapphon <sapphon@gmail.com>
2023-07-10 23:00:48 -0400

    Attempt to place two-way elevator pivots in their centers, for rotational ease
    SwapTransformsConsequence seems to work as well

---
+++ Assets/SwapTransformsConsequence.cs
@@ -0,0 +1,51 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using Consequences;
+using Triggers;
+using UnityEngine;
+
+public class SwapTransformsConsequence : AbstractCancelableConsequence
+{
+    public Transform toSwap;
+    public Transform toSwapWith;
+    protected TransformData original;
+
+    public override void Execute(TriggerData? data)
+    {
+        performSwap();
+    }
+
+    protected void performSwap()
+    {
+        original = new TransformData(toSwap);
+        new TransformData(toSwapWith).ToTransform(toSwap);
+        original.ToTransform(toSwapWith);
+    }
+
+    public override void Cancel(TriggerData? data)
+    {
+        performSwap();
+    }
+}
+
+public class TransformData
+{
+    private Vector3 position;
+    private Quaternion rotation;
+    private Vector3 scale;
+
+    public TransformData(Transform transform)
+    {
+        this.position = transform.position;
+        this.rotation = transform.rotation;
+        this.scale = transform.localScale;
+    }
+
+    public void ToTransform(Transform transform)
+    {
+        transform.position = position;
+        transform.rotation = rotation;
+        transform.localScale = scale;
+    }
+}
