62ae87b979586bc73a2810fe3cb8eb07a6a6b227
Sapphon <sapphon@gmail.com>
2023-09-03 18:41:28 -0400

    Add security room to upstairs
    Create 4-way toggle with lights and text for security room

---
+++ Assets/Scripts/Consequences/SetLightColorConsequence.cs
@@ -0,0 +1,16 @@
+#nullable enable
+using Triggers;
+using UnityEngine;
+
+namespace Consequences
+{
+    public class SetLightColorConsequence : ToggleLightColorConsequence
+    {
+        public override void Execute(TriggerData? data)
+        {
+            if (!lightToToggle) return;
+
+                this.lightToToggle.color = goingTo;
+            }
+    }
+}
